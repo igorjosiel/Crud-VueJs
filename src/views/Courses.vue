bb923919a0607772fc0e8f35e3c9ccd4bd491832
Igor Josiel <igorjosielvitalino@gmail.com>
2023-06-09 23:26:43 -0300

    feat: criando novo arquivo para ser a página de cursos.

---
+++ src/views/Courses.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    Courses
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CoursesView',
+});
+</script>
