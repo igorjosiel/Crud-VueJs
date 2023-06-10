30c1e1f085b5daf63af2a66f912d59c904e27aea
Igor Josiel <igorjosielvitalino@gmail.com>
2023-06-09 23:27:38 -0300

    feat: acrescentando uma rota ao arquivo de roteamento do projeto.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,16 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import StudentsView from '../views/Students.vue';
+import Courses from '../views/Courses.vue';
 
 const routes: RouteRecordRaw[] = [{
     path: '/',
     name: 'Students',
     component: StudentsView,
+},
+{
+    path: '/courses',
+    name: 'Courses',
+    component: Courses
 }];
 
 const router = createRouter({
