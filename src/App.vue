aa5c686d321e2b7cf1a8ceca7664af28c5883ec6
Igor Josiel <igorjosielvitalino@gmail.com>
2023-06-15 18:51:47 -0300

    fix: externalizando os estilos globais da aplicação.

--- src/App.vue
+++ src/App.vue
@@ -20,3 +20,46 @@ export default defineComponent({
   },
 });
 </script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  table {
+    width: 100%;
+  }
+
+  .input-margin {
+      margin: 1%;
+  }
+
+  .button-margin {
+    margin: 1%;
+  }
+
+  .table th:not([align])  {
+    text-align: center;
+  }
+
+  .font-bold {
+    font-weight: 600;
+  }
+
+  .input-name, .input-course {
+    width: 40%;
+  }
+
+  .input-phoneNumber {
+    width: 20%;
+  }
+
+  .input-age, .input-duration {
+    width: 20%;
+  }
+</style>
