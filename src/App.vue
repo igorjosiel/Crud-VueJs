1d56a1c6d7e69c72a8b47196b593dff53399d069
Igor Josiel <igorjosielvitalino@gmail.com>
2023-06-15 17:14:54 -0300

    fix: externalizando a barra de navegação para um arquivo específico só pra isso.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <template>
   <div class="columns">
     <div class="column is-one-fifth">
-      <nav class="navbar" role="navigation" aria-label="main navigation">
-        <ul class="navbar-brand">
-          <li>
-            <router-link to="/students" class="navbar-item">
-              Alunos
-            </router-link>
-
-            <hr class="navbar-divider">
-            
-            <router-link to="/courses" class="navbar-item">
-              Cursos
-            </router-link>
-          </li>
-        </ul>
-      </nav>
+      <NavBar />
     </div>
     <div class="column is-four-fifth">
       <router-view></router-view>
@@ -25,28 +11,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import NavBar from './components/NavBar.vue';
 
 export default defineComponent({
   name: 'App',
+  components: {
+    NavBar
+  },
 });
 </script>
-
-<style scoped>
-  nav {
-    background: lightgray;
-  }
-
-  .navbar-brand {
-    justify-content: center;
-  }
-
-  .navbar-divider {
-    display: block;
-    background: #3e8ed0;
-  }
-
-  .navbar, .navbar-end, .navbar-menu, .navbar-start {
-    display: flex;
-    justify-content: center;
-  }
-</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <ul class="navbar-brand">
+            <router-link to="/students" class="navbar-item">
+                Alunos
+            </router-link>
+
+            <hr class="navbar-divider">
+
+            <router-link to="/courses" class="navbar-item">
+                Cursos
+            </router-link>
+        </ul>
+    </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TableData',
+});
+</script>
+
+<style scoped>
+  nav {
+    background: lightgray;
+  }
+
+  .navbar-brand {
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .navbar-divider {
+    display: block;
+    background: #3e8ed0;
+  }
+
+  .navbar, .navbar-end, .navbar-menu, .navbar-start {
+    display: flex;
+    justify-content: center;
+  }
+</style>
