ced2e7a2a79a54775204c2211b95b8646718a474
Igor Josiel <igorjosielvitalino@gmail.com>
2023-06-16 14:21:21 -0300

    fix: ajustando as mutações para adicionar ou alterar dados.

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,9 +8,31 @@ export const store = createStore({
         courses: [] as ICourses[],
     },
     mutations: {
-        'ADD_STUDENT'(state, newStudent: IStudents) {
-            state.students.push(newStudent);
-        }
+        'ADD_CHANGE_STUDENT'(state, newDataStudent: IStudents) {
+            const studentFoundIndex = state.students.findIndex(student => student.id === newDataStudent.id);
+
+            // Se o índece do item for diferente de -1 signica que é uma alteração no item encontrado
+            if (studentFoundIndex !== -1) {
+                state.students[studentFoundIndex] = newDataStudent;
+        
+                return;
+            }
+
+            state.students.push(newDataStudent);
+        },
+        
+        'ADD_CHANGE_COURSE'(state, newDataCourse: ICourses) {
+            const courseFoundIndex = state.courses.findIndex(course => course.id === newDataCourse.id);
+
+            // Se o índece do item for diferente de -1 signica que é uma alteração no item encontrado
+            if (courseFoundIndex !== -1) {
+                state.courses[courseFoundIndex] = newDataCourse;
+        
+                return;
+            }
+
+            state.courses.push(newDataCourse);
+        },
     }
 });
 
