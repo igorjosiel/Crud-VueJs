eb5e248e87e526c86146e8ab264a9a06a9e265e3
Igor Josiel <igorjosielvitalino@gmail.com>
2023-06-16 13:42:44 -0300

    feat: adicionando a primeira mutação ao projeto para adicionar estudantes.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,27 +1,17 @@
 import { createStore, Store } from "vuex";
 import { InjectionKey } from 'vue';
-import { IStore } from "@/interfaces";
+import { IStore, IStudents, ICourses } from "@/interfaces";
 
 export const store = createStore({
     state: {
-        students: [{
-            id: 0,
-            name: 'Igor Josiel',
-            phoneNumber: '(32) 999345623',
-            age: 26,
-        },
-        {
-            id: 1,
-            name: 'Franciele de Paula',
-            phoneNumber: '(32) 999345623',
-            age: 23,
-        }],
-        courses: [{
-            id: 0,
-            course: 'Sistemas de Informação',
-            duration: 2,
-        }],
+        students: [] as IStudents[],
+        courses: [] as ICourses[],
     },
+    mutations: {
+        'ADD_STUDENT'(state, newStudent: IStudents) {
+            state.students.push(newStudent);
+        }
+    }
 });
 
 export const key: InjectionKey<Store<IStore>> = Symbol();
